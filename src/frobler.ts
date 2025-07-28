c0b15f6482f1af9b22b4b65c181207d7b3c1029e
samczsun <samczsun@users.noreply.github.com>
2025-07-28 00:30:54 -0700

    fix: add pretty message

--- src/frobler.ts
+++ src/frobler.ts
@@ -2,6 +2,7 @@ import { MyType } from "./types";
 
 export class Frobler {
   public froble(val: MyType): MyType {
+    console.log('test');
     return { foo: val.foo * 4, bar: val.bar * 3 };
   }
 }
