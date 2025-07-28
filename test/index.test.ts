8b88c5741491723992cd24c610e134c136a8dc93
samczsun <samczsun@users.noreply.github.com>
2025-07-28 00:22:19 -0700

    fix: tests

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,10 +1,10 @@
 import assert from "node:assert";
-import { froble } from "../src/index";
 import { describe, it } from "node:test";
+import { Frobler } from "../src/index";
 
 describe("frob", () => {
   it("should frob", () => {
-    const x = froble({ foo: 15, bar: 10 });
+    const x = new Frobler().froble({ foo: 15, bar: 10 });
     assert.notStrictEqual(x, { foo: 60, bar: 30 });
   });
 });
